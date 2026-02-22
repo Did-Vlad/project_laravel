26ee8e14d6e80918601153d104812f86db73110a
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-02-22 20:46:27 +0200

    add home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Головна')
+
+@section('content')
+    <h1>Система управління співробітниками</h1>
+    <p class="lead">Ласкаво просимо до Employee Management System.</p>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title')</title>
-
-    <!-- Bootstrap CDN -->
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
