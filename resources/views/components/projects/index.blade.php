a56e0f835798b59326d6c99d80ab05888cca24e9
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-02-25 17:21:26 +0200

    add inde.php

---
+++ resources/views/components/projects/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Проекти</h1>
+
+<a href="/projects/create" class="btn btn-success mb-3">
+    Додати проект
+</a>
+
+@foreach($projects as $project)
+    <div class="card p-3 mb-3">
+        <h4>{{ $project->name }}</h4>
+        <p>{{ $project->description }}</p>
+        <p>Бюджет: {{ $project->budget }}</p>
+
+        <a href="/projects/{{ $project->id }}">
+            Детальніше
+        </a>
+    </div>
+@endforeach
+
+@endsection
