c5f4d89e00318c2f6674c09524b8c1b0361f2a78
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-02-25 17:22:01 +0200

    index.php emoployee

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,23 +1,19 @@
-@extends('layout')
+@extends('layouts.app')
+
+@section('title', 'Співробітники')
 
 @section('content')
+    <h2>Список співробітників</h2>
 
-<h1>Проекти</h1>
-
-<a href="/projects/create" class="btn btn-success mb-3">
-    Додати проект
-</a>
-
-@foreach($projects as $project)
-    <div class="card p-3 mb-3">
-        <h4>{{ $project->name }}</h4>
-        <p>{{ $project->description }}</p>
-        <p>Бюджет: {{ $project->budget }}</p>
-
-        <a href="/projects/{{ $project->id }}">
-            Детальніше
-        </a>
+    <div class="row">
+        @foreach($employees as $employee)
+            <div class="col-md-4">
+                <x-card 
+                    :title="$employee['first_name'] . ' ' . $employee['last_name']"
+                    :subtitle="$employee['position']"
+                    :link="'/employees/' . $employee['id']"
+                />
+            </div>
+        @endforeach
     </div>
-@endforeach
-
 @endsection
