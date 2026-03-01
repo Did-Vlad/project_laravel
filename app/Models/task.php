6665d380e725097d9e84c0d98fcf7bff3f5a2c1d
VladyslavDidenko <steam.gaming.prvt@gmail.com>
2026-03-01 11:37:20 +0200

    add value in task

--- app/Models/task.php
+++ app/Models/task.php
@@ -4,7 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class task extends Model
+class Task extends Model
 {
-    
+    protected $fillable = [
+        'name',
+        'description',
+        'start_date',
+        'end_date',
+        'project_id',
+        'employee_id',
+        'status'
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
