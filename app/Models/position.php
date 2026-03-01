08ef59373ccbd3ff18fd2af20414ce55a78daa37
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-03-01 09:32:42 +0200

    make position model

---
+++ app/Models/position.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class position extends Model
+{
+  protected $table = 'position';
+   
+  public function employees()
+  {
+    return $this->hasMany(Employee::class);
+}
+}
