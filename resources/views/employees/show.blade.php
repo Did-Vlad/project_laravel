e38426281f6696fbfbd51f785da8f31f5230822f
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-03-01 10:44:12 +0200

    add value

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -5,10 +5,17 @@
 @section('content')
 <h1>{{ $employee->last_name }} {{ $employee->first_name }}</h1>
 
+<p>По батькові: {{ $employee->midl_name }}</p>
+<p>Стать: {{ $employee->gender }}</p>
+
 <p>Email: {{ $employee->email }}</p>
 <p>Телефон: {{ $employee->phone }}</p>
+
+<p>Дата прийому: {{ $employee->hire_date }}</p>
+<p>Дата звільнення: {{ $employee->termination_date ?? '—' }}</p>
+
 <p>Статус: {{ $employee->status }}</p>
-<p>Посада: {{ $employee->position->name ?? 'Невідомо' }}</p>
+<p>Посада: {{ $employee->position->name ?? '' }}</p>
 
 <a href="/employees">Назад</a>
 @endsection
