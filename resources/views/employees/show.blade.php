6719ff1f0f1ae7e6b5a558d66eaf7264e5391248
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-03-01 09:49:34 +0200

    add new blade

---
+++ resources/views/employees/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Працівник')
+
+@section('content')
+<h1>{{ $employee->last_name }} {{ $employee->first_name }}</h1>
+
+<p>Email: {{ $employee->email }}</p>
+<p>Телефон: {{ $employee->phone }}</p>
+<p>Статус: {{ $employee->status }}</p>
+<p>Посада: {{ $employee->position->title ?? 'Невідомо' }}</p>
+
+<a href="/employees">Назад</a>
+@endsection
