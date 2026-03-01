611b5d387c24caeb3d09c9d2f09ac9a8037b2661
VladyslavDidenko <steam.gaming.prvt@gmail.com>
2026-03-01 11:29:05 +0200

    add ProjectAssignments value

--- app/Models/ProjectAssignment.php
+++ app/Models/ProjectAssignment.php
@@ -4,7 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class projectassignment extends Model
+class ProjectAssignment extends Model
 {
-    //
+    protected $table  = 'project_assignments';
+    protected $fillable = [
+        'employee_id',
+        'position_id',
+        'start_date',
+        'end_date',
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+ public function position()
+    {
+        return $this->belongsTo(Position::class);
+    }
 }
