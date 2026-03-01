3e56fa47593c1cbeffd80f166afd45c3f27b817c
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-03-01 15:06:32 +0200

    make spaces

--- resources/views/admin/employees/index.blade.php
+++ resources/views/admin/employees/index.blade.php
@@ -1,7 +1,7 @@
 <td>
     <a href="{{route('admin.employees.show', $employee)}}">Переглянути</a>
 
-    <form action="{{route('admin.empoyee.destroy', $employee)}}" method="POST" style="display:inline;">
+    <form action="{{route('admin.empoyees.destroy', $employee)}}" method="POST" style="display:inline;">
     @csrf
     @method('DELETE')
     <button type="submit" onclick="return confirm('Видалити цього співробітника?')">Видалити</button>
