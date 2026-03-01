5c56a3b659eba0861b8c586ae2a7b4df916bdeff
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-03-01 09:38:44 +0200

    change employee app

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    protected $table = 'employee';
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'midl_name',
+        'gender',
+        'phone',
+        'email',
+        'hire_date',
+        'termination_date',
+        'status',
+        'position_id',
+    ];
+    public function position()
+    {
+        return $this->belongsTo(position::class);
+    }
 }
