f7eee6ff57efc7773acaf888c6c469ff848c631a
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-03-01 14:26:49 +0200

    make admin employee

--- app/Http/Controllers/Admin/EmployeeController.php
+++ app/Http/Controllers/Admin/EmployeeController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Employee;
 
 class EmployeeController extends Controller
 {
-    //
+    public function index()
+    {
+        $employees = Employee::with('position', 'department')->get();
+        return view('admin.employees.index', compact('employees'));
+    }
+
+    public function show(Employee $employee)
+    {
+        $employee->load(['position', 'department', 'tasks', 'trainings', 'vacations', 'projectAssignments.position']);
+        return view('admin.employees.show', compact('employee'));
+    }
+
+    public function destroy(Employee $employee)
+    {
+        $employee->delete();
+        return redirect()->route('admin.employees.index')->with('success', 'Працівника видалено');
+    }
 }
