ff6a5e94f1a72f33d74380aab05e66d5f9c4ed20
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-03-11 12:12:43 +0200

    add p

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -12,41 +12,47 @@
 
 <form action="/projects" method="POST">
     @csrf
-
+<p>
     <div>
         <label>Назва</label>
         <input type="text" name="name" value="{{ old('name') }}">
         @error('name') <span style="color:red;">{{ $message }}</span> @enderror
     </div>
+</p>
 
+<p>
     <div>
         <label>Опис</label>
         <textarea name="description">{{ old('description') }}</textarea>
         @error('description') <span style="color:red;">{{ $message }}</span> @enderror
     </div>
-
+</p>
+<p>
     <div>
         <label>Бюджет</label>
         <input type="number" name="cost" value="{{ old('cost') }}" step="0.01">
         @error('cost') <span style="color:red;">{{ $message }}</span> @enderror
     </div>
-
+</p>
+<p>
     <div>
         <label>Дата початку</label>
         <input type="date" name="start_date" value="{{ old('start_date') }}">
         @error('start_date') <span style="color:red;">{{ $message }}</span> @enderror
     </div>
-
+</p>
+<p>
     <div>
         <label>Дата кінця</label>
         <input type="date" name="end_date" value="{{ old('end_date') }}">
         @error('end_date') <span style="color:red;">{{ $message }}</span> @enderror
     </div>
-
+</p>
+<p>
     <br>
     <button type="submit">Підтвердити</button>
     <a href="/projects">Скасувати</a>
-
+</p>
 </form>
 
 @endsection
