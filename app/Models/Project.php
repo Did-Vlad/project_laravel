4d0217a4a7cda7a4854de3ddbad59c2a0cf4065d
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-03-11 11:38:41 +0200

    wrote Task to Project.php

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,6 +8,8 @@ class Project extends Model
 {
     protected $table = 'project';
 
+    public $timestamps = false;
+
     protected $fillable = [
         'name',
         'description',
@@ -15,4 +17,9 @@ class Project extends Model
         'end_date',
         'cost',
     ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'project_id');
+    }
 }
