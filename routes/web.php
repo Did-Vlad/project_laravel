9d9dbb4e313fbceabeb4a610c1de55af08d4110b
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-03-15 11:28:10 +0200

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MainController;
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\ProjectController;
 
-Route::get('/', [MainController::class, 'index']);
-
-Route::get('/about', [MainController::class, 'about']);
-
-Route::get('/employees', [EmployeeController::class, 'index']);
-
-Route::get('/employees/{id}', [EmployeeController::class, 'show']);
-
-Route::get('/projects', [ProjectController::class, 'index']);
-
-Route::get('/projects/create', [ProjectController::class, 'create']);
-
-Route::post('/projects', [ProjectController::class, 'store']);
-
-Route::get('/projects/{id}', [ProjectController::class, 'show']);
-
-// Адмін маршрути
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::resource('employees', \App\Http\Controllers\Admin\EmployeeController::class)
-        ->only(['index', 'show', 'destroy', 'create', 'store']);
-
-    Route::resource('projects', \App\Http\Controllers\Admin\ProjectController::class)
-        ->only(['index']);
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
