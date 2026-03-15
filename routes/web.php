d2003d7bece6c50123bd350d39f2646c637dcd54
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-03-15 11:57:30 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,18 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ProjectController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MainController::class, 'index']);
+Route::get('/about', [MainController::class, 'about']);
+Route::get('/employees', [EmployeeController::class, 'index']);
+Route::get('/employees/{id}', [EmployeeController::class, 'show']);
+Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/projects/create', [ProjectController::class, 'create']);
+Route::post('/projects', [ProjectController::class, 'store']);
+Route::get('/projects/{id}', [ProjectController::class, 'show']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +23,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// адмін маршрути
+Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::resource('employees', \App\Http\Controllers\Admin\EmployeeController::class)
+        ->only(['index', 'show', 'destroy', 'create', 'store']);
+
+    Route::resource('projects', \App\Http\Controllers\Admin\ProjectController::class)
+        ->only(['index']);
 });
 
 require __DIR__.'/auth.php';
