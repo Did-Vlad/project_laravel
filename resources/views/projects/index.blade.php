710ed2e2fbdf1efe6c959d7f2efe6d2e3762523b
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-03-15 13:56:15 +0200

    rewrite project index.blade

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.app')
 
+@section('title', 'Проекти')
+
 @section('content')
 
-<h1>Проекти</h1>
-
-<a href="/projects/create" class="btn btn-success mb-3">
-    Додати проект
-</a>
+<div class="d-flex justify-content-between align-items-center mb-4">
+    <h1>Проекти</h1>
+    <a href="/projects/create" class="btn btn-success">Додати проект</a>
+</div>
 
 @foreach($projects as $project)
-    <div class="card p-3 mb-3">
-        <h4>{{ $project->name }}</h4>
-        <p>{{ $project->description }}</p>
-        <p>Бюджет: {{ $project->budget }}</p>
-
-        <a href="/projects/{{ $project->id }}">
-            Детальніше
-        </a>
+    <div class="card mb-3">
+        <div class="card-body">
+            <h4 class="card-title fw-bold">{{ $project->name }}</h4>
+            <p class="card-text text-muted">{{ $project->description }}</p>
+            <p class="card-text">Бюджет: {{ $project->budget }}</p>
+            <a href="/projects/{{ $project->id }}" class="btn btn-primary btn-sm">Детальніше</a>
+        </div>
     </div>
 @endforeach
 
