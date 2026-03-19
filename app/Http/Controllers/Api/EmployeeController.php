a98c97fc4e38256aab2b6ea43d45d43815cdc0c2
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-03-19 10:32:53 +0200

    add list all emloyees in json format

--- app/Http/Controllers/Api/EmployeeController.php
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -4,46 +4,28 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Employee;
 class EmployeeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+       $employees = Employee::with('position', 'department')->get();
+       return response()->json($employees);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+   
+    public function show($id)
     {
-        //
-    }
+        $employee = Employee::with([
+            'position',
+            'department',
+            'task',
+            'trainings',
+            'vacations',
+            'projectAssignments',
+        ])->findOrFail($id);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json($employee);
     }
 }
