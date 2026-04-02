66b558b4f7fb2696d316972489152c21420d9fa4
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-04-02 09:39:14 +0300

    change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'EMS')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 <body>
 
